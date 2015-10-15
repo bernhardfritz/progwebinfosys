f6a0f1d48970c6489b909ef06c02d5421a3968f0
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2015-10-15 13:36:45 +0200

    asdf

---
+++ task02/beitrag.php
@@ -0,0 +1,34 @@
+<link rel='stylesheet' href='css/style.css'>
+<div id='beitrag'>
+  <div id='blogmenu'>
+    <ul>
+      <li><a href='#'>menuitem1</a></li>
+      <li><a href='#'>menuitem2</a></li>
+    </ul>
+  </div>
+  <div id='blogcontent'>
+    <h1>
+      <a href='#'>#</a>title
+    </h1>
+    <div>
+      <span class='left'>
+        by <a href='#'>author</a>
+      </span>
+      <span class='right'>
+        <div>created on 01.01.2015</div>
+      </span>
+    </div>
+    <p id='blogtext'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    <div>
+      <span class='left'>
+        <div id='keyword'>
+          <a href='#'>keyword1</a>
+          <a href='#'>keyword2</a>
+        </div>
+      </span>
+      <span class='right'>
+        <div>updated on 01.01.2015</div>
+      </span>
+    </div>
+  </div>
+</div>
