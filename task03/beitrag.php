5d7d4db24c3e2181671d8581e405de3914e4fd2b
zmario <mario.zelger@aon.at>
2015-10-22 13:37:25 +0200

    bbcode replacement fix

--- task03/beitrag.php
+++ task03/beitrag.php
@@ -6,6 +6,53 @@
   $keywords = $posting->getKeywords();
   $createdOn = $posting->getCreated();
   $updatedOn = $posting->getUpdated();
+
+  if (!function_exists('bbCodeReplacement')) {
+    function bbCodeReplacement($text) {
+      $text = fixBBCode($text);
+      $text = preg_replace("/\[b\](.*)\[\/b\]/Usi", "<b>\\1</b>", $text);   // bold
+      $text = preg_replace("/\[i\](.*)\[\/i\]/Usi", "<i>\\1</i>", $text);   // italic
+      $text = preg_replace("/\[u\](.*)\[\/u\]/Usi", "<u>\\1</u>", $text);   // underlined
+      $text = preg_replace("/\\n/Usi", "<br />", $text);   // new line
+      $text = preg_replace("/\[color=(.*)\](.*)\[\/color\]/Usi", "<font color=\"\\1\">\\2</font>", $text);  // text color
+      $text = preg_replace("/\[size=(.*)\](.*)\[\/size\]/Usi", "<font size=\"\\1\">\\2</font>", $text);  // text size
+      $text = preg_replace("/\[font=(.*)\](.*)\[\/font\]/Usi", "<span style=\"font-family:\\1\">\\2</span>", $text);  // text face
+      $text = preg_replace("/\[url=(.*)\](.*)\[\/url\]/Usi", "<u><a href=\"\\1\" class=\"nohover\" style=\"font-weight:normal\">\\2</a></u>", $text);  // link
+      $text = preg_replace("/\[h1\](.*)\[\/h1\]/Usi", "<h1>\\1</h1>", $text);   // h1
+      $text = preg_replace("/\[h2\](.*)\[\/h2\]/Usi", "<h2>\\1</h2>", $text);   // h2
+      $text = preg_replace("/\[h3\](.*)\[\/h3\]/Usi", "<h3>\\1</h3>", $text);   // h3
+      $text = preg_replace("/\[h4\](.*)\[\/h4\]/Usi", "<h4>\\1</h4>", $text);   // h4
+      $text = preg_replace("/\[h5\](.*)\[\/h5\]/Usi", "<h5>\\1</h5>", $text);   // h5
+      $text = preg_replace("/\[h6\](.*)\[\/h6\]/Usi", "<h6>\\1</h6>", $text);   // h6
+
+      return $text;
+    }
+  }
+
+  if (!function_exists('fixBBCode')) {
+    function fixBBCode($str) {
+      $result = $str;
+
+      $taglist = array('b', 'i', 'u', 'color', 'size', 'font', 'url', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6');
+      $result_array = array();
+
+      foreach($taglist as $tag) {
+          preg_match_all ('/\['.$tag.'(=[^ ]+)?\]/i', $str, $matches);
+          $opentags = count($matches['0']);
+
+          preg_match_all ('/\[\/'.$tag.'\]/i', $str, $matches);
+          $closetags = count($matches['0']);
+
+          $unclosed = $opentags - $closetags;
+
+          for ($i = 0; $i < $unclosed; $i++) {
+            $result = $result . '[/' . $tag . ']';
+          }
+      }
+
+      return $result;
+    }
+  }
 ?>
 <div id='beitrag'>
   <div id='blogmenu'>
@@ -47,26 +94,10 @@
     </div>
     <p id='blogtext'>
       <?php
-        // BBCode replacement
-        $text = preg_replace("/\[b\](.*)\[\/b\]/Usi", "<b>\\1</b>", $text);   // bold
-        $text = preg_replace("/\[i\](.*)\[\/i\]/Usi", "<i>\\1</i>", $text);   // italic
-        $text = preg_replace("/\[u\](.*)\[\/u\]/Usi", "<u>\\1</u>", $text);   // underlined
-        $text = preg_replace("/\\n/Usi", "<br />", $text);   // underlined
-        $text = preg_replace("/\[color=(.*)\](.*)\[\/color\]/Usi", "<font color=\"\\1\">\\2</font>", $text);  // text color
-        $text = preg_replace("/\[size=(.*)\](.*)\[\/size\]/Usi", "<font size=\"\\1\">\\2</font>", $text);  // text size
-        $text = preg_replace("/\[font=(.*)\](.*)\[\/font\]/Usi", "<span style=\"font-family:\\1\">\\2</span>", $text);  // text face
-        $text = preg_replace("/\[url=(.*)\](.*)\[\/url\]/Usi", "<u><a href=\"\\1\" class=\"nohover\">\\2</a></u>", $text);  // link
-        $text = preg_replace("/\[h1\](.*)\[\/h1\]/Usi", "<h1>\\1</h1>", $text);   // h1
-        $text = preg_replace("/\[h2\](.*)\[\/h2\]/Usi", "<h2>\\1</h2>", $text);   // h2
-        $text = preg_replace("/\[h3\](.*)\[\/h3\]/Usi", "<h3>\\1</h3>", $text);   // h3
-        $text = preg_replace("/\[h3\](.*)\[\/h3\]/Usi", "<h3>\\1</h3>", $text);   // h4
-        $text = preg_replace("/\[h3\](.*)\[\/h3\]/Usi", "<h3>\\1</h3>", $text);   // h5
-        $text = preg_replace("/\[h3\](.*)\[\/h3\]/Usi", "<h3>\\1</h3>", $text);   // h6
-
-        if(!isset($postings) || strlen($text) <= 300) {
-          echo $text;
+        if(!isset($postings) || strlen($text) <= 200) {
+          echo bbCodeReplacement($text);
         } else {
-          echo substr($text, 0, 300) . "... <a href='index.php?id=$id'>(read more)</a>";
+          echo bbCodeReplacement(substr($text, 0, 200)) . "... <a href='index.php?id=$id'>(read more)</a>";
         }
       ?>
     </p>
