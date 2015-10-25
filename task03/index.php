747286ad85ce6b0f0fb3905a17f110945acf67bd
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2015-10-25 16:35:55 +0100

    loginbutton

--- task03/index.php
+++ task03/index.php
@@ -26,12 +26,12 @@
               <?php
                 echo "Hello, " . $loggedInUser->getUsername() . "&nbsp;";
                 if($loggedInUser->getId() != 4) {
-                  echo "<a href='Logout.php' title='logout' class='loginbutton'>logout</a>";
+                  echo "<a href='Logout.php' title='logout' class='loginbutton'><img src='img/logout.png' width='20px' height='20px'></a>";
                   if($loggedInUser->getWritePost()) {
                     echo "<a href='CreatePost.php' title='create post'>&nbsp;+</a>";
                   }
                 } else {
-                  echo "<a href='#' onClick='login()' title='login' class='loginbutton'>login</a>";
+                  echo "<a href='#' onClick='login()' class='loginbutton' title='login'><img src='img/login.png' width='20px' height='20px'></img></a>";
                 }
               ?>
             </div>
