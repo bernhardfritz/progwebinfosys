76144411cde9bf6fabbd6a69eaf1c766c9f64438
zmario <mario.zelger@aon.at>
2015-11-15 11:46:53 +0100

    unit test update

--- WebShop/src/test/java/unit/model/PrivilegesTest.java
+++ WebShop/src/test/java/unit/model/PrivilegesTest.java
@@ -9,90 +9,126 @@ import model.Privileges;
 public class PrivilegesTest {
 
 	@Test
-	public void testCategoryRead() {
+	public void testCategoryReadOnly() {
 		Privileges privileges = new Privileges(100000000);
 		
 		assertTrue(privileges.isCategoryRead());
+		assertFalse(privileges.isCategoryWrite());
+		assertFalse(privileges.isCategoryDelete());
 	}
 	
 	@Test
-	public void testCategoryWrite() {
+	public void testCategoryWriteOnly() {
 		Privileges privileges = new Privileges(10000000);
 		
+		assertFalse(privileges.isCategoryRead());
 		assertTrue(privileges.isCategoryWrite());
+		assertFalse(privileges.isCategoryDelete());
 	}
 	
 	@Test
-	public void testCategoryDelete() {
+	public void testCategoryDeleteOnly() {
 		Privileges privileges = new Privileges(1000000);
 		
+		assertFalse(privileges.isCategoryRead());
+		assertFalse(privileges.isCategoryWrite());
 		assertTrue(privileges.isCategoryDelete());
 	}
 	
 	@Test
-	public void testItemRead() {
+	public void testItemReadOnly() {
 		Privileges privileges = new Privileges(100000);
 		
 		assertTrue(privileges.isItemRead());
+		assertFalse(privileges.isItemWrite());
+		assertFalse(privileges.isItemDelete());
 	}
 	
 	@Test
-	public void testItemWrite() {
+	public void testItemWriteOnly() {
 		Privileges privileges = new Privileges(10000);
 		
+		assertFalse(privileges.isItemRead());
 		assertTrue(privileges.isItemWrite());
+		assertFalse(privileges.isItemDelete());
 	}
 	
 	@Test
-	public void testItemDelete() {
+	public void testItemDeleteOnly() {
 		Privileges privileges = new Privileges(1000);
 		
+		assertFalse(privileges.isItemRead());
+		assertFalse(privileges.isItemWrite());
 		assertTrue(privileges.isItemDelete());
 	}
 	
 	@Test
-	public void testItemCommentRead() {
+	public void testItemCommentReadOnly() {
 		Privileges privileges = new Privileges(100);
 		
 		assertTrue(privileges.isItemCommentRead());
+		assertFalse(privileges.isItemCommentWrite());
+		assertFalse(privileges.isItemCommentDelete());
 	}
 	
 	@Test
-	public void testItemCommentWrite() {
+	public void testItemCommentWriteOnly() {
 		Privileges privileges = new Privileges(10);
 		
+		assertFalse(privileges.isItemCommentRead());
 		assertTrue(privileges.isItemCommentWrite());
+		assertFalse(privileges.isItemCommentDelete());
 	}
 	
 	@Test
-	public void testItemCommentDelete() {
+	public void testItemCommentDeleteOnly() {
 		Privileges privileges = new Privileges(1);
 		
+		assertFalse(privileges.isItemCommentRead());
+		assertFalse(privileges.isItemCommentWrite());
 		assertTrue(privileges.isItemCommentDelete());
 	}
 	
 	@Test
-	public void testAllRead() {
+	public void testAllReadOnly() {
 		Privileges privileges = new Privileges(100100100);
 		
 		assertTrue(privileges.isCategoryRead());
 		assertTrue(privileges.isItemRead());
 		assertTrue(privileges.isItemCommentRead());
+		assertFalse(privileges.isCategoryWrite());
+		assertFalse(privileges.isItemWrite());
+		assertFalse(privileges.isItemCommentWrite());
+		assertFalse(privileges.isCategoryDelete());
+		assertFalse(privileges.isItemDelete());
+		assertFalse(privileges.isItemCommentDelete());
 	}
 	
 	@Test
-	public void testAllWrite() {
+	public void testAllWriteOnly() {
 		Privileges privileges = new Privileges(10010010);
 		
+		assertFalse(privileges.isCategoryRead());
+		assertFalse(privileges.isItemRead());
+		assertFalse(privileges.isItemCommentRead());
 		assertTrue(privileges.isCategoryWrite());
 		assertTrue(privileges.isItemWrite());
 		assertTrue(privileges.isItemCommentWrite());
+		assertFalse(privileges.isCategoryDelete());
+		assertFalse(privileges.isItemDelete());
+		assertFalse(privileges.isItemCommentDelete());
 	}
 	
 	@Test
-	public void testAllDelete() {
+	public void testAllDeleteOnly() {
 		Privileges privileges = new Privileges(1001001);
 		
+		assertFalse(privileges.isCategoryRead());
+		assertFalse(privileges.isItemRead());
+		assertFalse(privileges.isItemCommentRead());
+		assertFalse(privileges.isCategoryWrite());
+		assertFalse(privileges.isItemWrite());
+		assertFalse(privileges.isItemCommentWrite());
 		assertTrue(privileges.isCategoryDelete());
 		assertTrue(privileges.isItemDelete());
 		assertTrue(privileges.isItemCommentDelete());
